d7f6df92b0ead6b8b767a2b9f0e458f30540ea72
C0DIC <michaellivesforever23@gmail.com>
2023-06-17 19:06:24 +0400

    Новые методы lower, swpcase, upper, eq New methods lower, swpcase, upper, eq

--- cstr.h
+++ cstr.h
@@ -24,6 +24,24 @@ Capitalize string
 char *cstr_capitalize(char *string);
 
 /*
+Возвращает строку с обратным регистром для каждого символа
+Swaping uppercase to lowercase and vice versa
+*/
+char *cstr_swpcase(char *string);
+
+/*
+Возвращает строку в нижнем регистре
+Returns string in lowercase
+*/
+char *cstr_lower(char *string);
+
+/*
+Возвращает строку в верхнем регистре
+Returns string in uppercase
+*/
+char *cstr_upper(char *string);
+
+/*
 Возвращает 1, если символ есть в строке, иначе 0
 Returns 1 if char in string, 0 otherwise
 */
@@ -36,6 +54,18 @@ Returns the lowest index in string where char is found. If no char, returns -1
 long long cstr_find(char *string, char character);
 
 /*
+Возвращает 1, если строки совпадают, иначе 0
+Returns 1 if strings are the same, 0 otherwise
+*/
+short cstr_eq(char *string, char *s_string);
+
+/*
+Сложение двух строк
+Concatenate two strings
+*/
+void cstr_cat(char *string, char *s_string);
+
+/*
 Возвращает 1, если вся строка в нижнем регистре, иначе 0
 Returns 1 if string in lowercase, 0 otherwise
 */
@@ -46,7 +76,6 @@ short cstr_islower(char *string);
 Returns 1 if string in uppercase, 0 otherwise
 */
 short cstr_isupper(char *string);
-
 
 
 #ifdef CSTR_IMPLEMENTATION
@@ -70,6 +99,34 @@ char *cstr_capitalize(char *string) {
     return string;
 }
 
+// Возвращает строку с обратным регистром для каждого символа
+char *cstr_swpcase(char *string) {
+    for (CSTR_INT i = 0; i < cstr_len(string); i++) {
+        if (string[i] >= 'a' && string[i] <= 'z') string[i] = string[i] - 32;
+        else if (string[i] >= 'A' && string[i] <= 'Z') string[i] = string[i] + 32;
+    }
+
+    return string;
+}
+
+// Возвращает строку в нижнем регистре
+char *cstr_lower(char *string) {
+    for (CSTR_INT i = 0; i < cstr_len(string); i++) {
+        if (string[i] >= 'A' && string[i] <= 'Z') string[i] = string[i] + 32;
+    }
+
+    return string;
+}
+
+// Возвращает строку в верхнем регистре
+char *cstr_upper(char *string) {
+    for (CSTR_INT i = 0; i < cstr_len(string); i++) {
+        if (string[i] >= 'a' && string[i] <= 'z') string[i] = string[i] - 32;
+    }
+
+    return string;
+}
+
 // Возвращает истину, если символ есть в строке, иначе ложь
 short cstr_has(char *string, char character) {
     while (*string) {
@@ -87,6 +144,23 @@ long long cstr_find(char *string, char character) {
     }
 
     return -1;
+}
+
+// Возвращает 1, если строки совпадают, иначе 0
+short cstr_eq(char *string, char *s_string) {
+    if (cstr_len(string) != cstr_len(s_string)) return CSTR_FALSE;
+
+    for (CSTR_INT i = 0; i < cstr_len(string); i++) {
+        if (string[i] != s_string[i]) return CSTR_FALSE;
+    }
+
+    return CSTR_TRUE;
+}
+
+// Сложение двух строк
+void cstr_cat(char *string, char *s_string) {
+    printf("<ERROR>: Not implemented yet\n");
+    return;
 }
 
 // Возвращает 1, если вся строка в верхнем регистре, иначе 0
